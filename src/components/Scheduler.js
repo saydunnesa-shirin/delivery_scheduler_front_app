5113e3f467936123426428f4656a9b72c59e8c45
saydunnesa-shirin <shirin071989@gmail.com>
2023-10-26 11:23:08 +0300

    added_post_call

--- src/components/Scheduler.js
+++ src/components/Scheduler.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import Button from 'react-bootstrap/Button';
 import { useDispatch, useSelector } from 'react-redux'
 import { getAllDeliverySchedule } from '../services/actions/schedularActions';
 
@@ -6,14 +7,38 @@ const Scheduler = () => {
 
     const dispatch = useDispatch();
     const {isLoading, schedulers, error} = useSelector((state) => state);
-    useEffect(() => {
-        dispatch(getAllDeliverySchedule());
+    
+    // useEffect(() => {
+    //     dispatch(getAllDeliverySchedule(postalCode, productId));
         
-    }, []);
+    // }, []);
+
+    const [postalCode, setPostalCode] = useState('');
+
+    const [productId, setProductId] = useState(0);
+
+    const handleChangePostalCode = (e) => {
+        setPostalCode(e.target.value);
+    } 
+
+    const handleChangeProductId = (e) => {
+        setProductId(e.target.value);
+    } 
+
+    const handleClick = () => {
+
+        dispatch(getAllDeliverySchedule(postalCode, productId));
+        
+    }
 
 
     return (
         <div>
+            <div className='SearchProduct'>
+                Postal Code: <input type='text' value={postalCode} onChange={handleChangePostalCode}></input>
+                Product Id: <input type='number' value={productId} onChange={handleChangeProductId}></input>
+                <button type='submit' onClick={handleClick}>Search</button>
+            </div>
             {isLoading && <h3>LOADING..</h3>}
             {error && <h3>{error.message}</h3>}
             <section>
--- src/services/actions/schedularActions.js
+++ src/services/actions/schedularActions.js
@@ -2,15 +2,17 @@ import axios from "axios";
 import { GET_DELEVERY_SCHEDULE, GET_DELEVERY_SCHEDULE_API_URL, GET_DELEVERY_SCHEDULE_FAILED, GET_DELEVERY_SCHEDULE_SUCCESS } from "../constants/schedularConstants";
 
 
-export const getAllDeliverySchedule = () => async(dispatch) => {
+export const getAllDeliverySchedule = (postalCode, productId) => async(dispatch) => {
     dispatch({type: GET_DELEVERY_SCHEDULE});
     try {
-        // axios.defaults.withCredentials = true;
-        // const response = await axios.get(GET_DELEVERY_SCHEDULE_API_URL);
+        
+        const response = await axios.post(GET_DELEVERY_SCHEDULE_API_URL, {
+            "postalCode": postalCode,
+            "productIds": [productId]
+        } , {mode:'cors'});
 
 
-
-        const response = await axios.get(GET_DELEVERY_SCHEDULE_API_URL, {mode:'cors'});
+        // const response = await axios.get(GET_DELEVERY_SCHEDULE_API_URL, {mode:'cors'});
 
         dispatch({type: GET_DELEVERY_SCHEDULE_SUCCESS, payload: response.data});
 
